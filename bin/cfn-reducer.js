701e0d5e1659465bcd00d61d8089ffae0f2b77c8
Sebastian Lundström <khueue@gmail.com>
2016-07-10 16:12:27 +0200

    Add command line runner

---
+++ bin/cfn-reducer.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var fs = require('fs');
+
+var CfnReducer = require('rfr')('/src/CfnReducer');
+
+var file = process.argv[2];
+if (!file) {
+	console.error('Usage: cfn-reducer.js <in.template> [Param1=Value1 ...]');
+	process.exit(1);
+}
+var template = JSON.parse(fs.readFileSync(file));
+
+var stackParams = {};
+
+process.argv.slice(3).forEach(function (pair) {
+	var parts = pair.split('=');
+	stackParams[parts[0]] = parts[1];
+});
+
+var reducer = new CfnReducer(template, stackParams);
+var reduced = reducer.reduce();
+
+var output = JSON.stringify(reduced, null, '\t');
+console.log(output);
