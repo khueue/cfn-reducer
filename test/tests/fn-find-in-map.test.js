7f4e9fe1acb284e3401081250091210f50b95343
Sebastian Lundström <khueue@gmail.com>
2016-08-19 09:37:55 +0200

    Add test for FindInMap error handling

--- test/tests/fn-find-in-map.test.js
+++ test/tests/fn-find-in-map.test.js
@@ -77,3 +77,105 @@ test('Fn::FindInMap - toggled off', function (t) {
 
 	t.deepEqual(reduced, expected);
 });
+
+test('Fn::FindInMap - bad map', function (t) {
+	t.plan(1);
+
+	var mappings = {
+		'my-map': {
+			'my-section': {
+				'my-key-1': 'my-value-1',
+				'my-key-2': 'my-value-2',
+				'my-key-3': 'my-value-3',
+			},
+		},
+	};
+
+	var template = {
+		Mappings: mappings,
+		thing: {
+			'Fn::FindInMap': [
+				'my-map-DOES-NOT-EXIST',
+				'my-section',
+				'my-key-2',
+			],
+		},
+	};
+
+	var reducer = new CfnReducer({
+		template: template,
+	});
+	var shouldThrow = function () {
+		reducer.reduce();
+	};
+
+	t.throws(shouldThrow);
+});
+
+test('Fn::FindInMap - bad map.section', function (t) {
+	t.plan(1);
+
+	var mappings = {
+		'my-map': {
+			'my-section': {
+				'my-key-1': 'my-value-1',
+				'my-key-2': 'my-value-2',
+				'my-key-3': 'my-value-3',
+			},
+		},
+	};
+
+	var template = {
+		Mappings: mappings,
+		thing: {
+			'Fn::FindInMap': [
+				'my-map',
+				'my-section-DOES-NOT-EXIST',
+				'my-key-2',
+			],
+		},
+	};
+
+	var reducer = new CfnReducer({
+		template: template,
+	});
+	var shouldThrow = function () {
+		reducer.reduce();
+	};
+
+	t.throws(shouldThrow);
+});
+
+test('Fn::FindInMap - bad map.section.key', function (t) {
+	t.plan(1);
+
+	var mappings = {
+		'my-map': {
+			'my-section': {
+				'my-key-1': 'my-value-1',
+				'my-key-2': 'my-value-2',
+				'my-key-3': 'my-value-3',
+			},
+		},
+	};
+
+	var template = {
+		Mappings: mappings,
+		thing: {
+			'Fn::FindInMap': [
+				'my-map',
+				'my-section',
+				'my-key-DOES-NOT-EXIST',
+			],
+		},
+	};
+
+	var reducer = new CfnReducer({
+		template: template,
+	});
+	var shouldThrow = function () {
+		reducer.reduce();
+	};
+
+	t.throws(shouldThrow);
+});
